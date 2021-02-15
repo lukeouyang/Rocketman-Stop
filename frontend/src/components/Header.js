149155c251a3e90fd856c4ddec902d62611266ee
lukeouyang <luke_ou@hotmail.com>
2021-02-14 16:56:24 -0800

    add routing facility

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 // import { useDispatch, useSelector } from 'react-redux'
-// import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 // import SearchBox from './SearchBox'
 // import { logout } from '../actions/userActions'
@@ -20,18 +20,18 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          {/* <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </LinkContainer> */}
+          <LinkContainer to='/'>
+            <Navbar.Brand>Rocketman StopStop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             {/* <Route render={({ history }) => <SearchBox history={history} />} /> */}
             <Nav className='ml-auto'>
-              {/* <LinkContainer to='/cart'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
-              </LinkContainer> */}
+              </LinkContainer>
               {/* {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
                   <LinkContainer to='/profile'>
