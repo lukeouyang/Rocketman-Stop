de5a990e642d5eb327217f0a089b26bc4ec1b182
lukeouyang <luke_ou@hotmail.com>
2021-02-16 03:31:04 -0800

    prepare for deployment

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>
-            Copyright &copy; Rocketman Stop
-          </Col>
+          <Col className='text-center py-3'>Copyright &copy; Rocketman Stop</Col>
         </Row>
       </Container>
     </footer>
